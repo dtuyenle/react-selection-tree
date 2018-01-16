ff5bdfcccbf41a0671680392ed64103526fd89e6
Tuyen <dtuyen.le@gmail.com>
2018-01-16 01:28:21 -0500

    add inner level button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,41 @@ import InputBox from 'components/InputBox';
 
 class App extends Component {
   render() {
+    const data = {
+      id: 0,
+      name: 'Tone',
+      children: {
+        negative: {
+          id: 'negative',
+          name: 'Negative',
+          children: {
+            negative1: {
+              id: 'negative1',
+              name: 'Negative1',
+              children: null,
+              parentId: 'negative'
+            },
+            negative2: {
+              id: 'negative2',
+              name: 'Negative2',
+              children: null,
+              parentId: 'negative'
+            }
+          },
+          parentId: 0
+        },
+        positive: {
+          id: 'positive',
+          name: 'Positive',
+          children: null,
+          parentId: 0
+        },
+        neutral: {id: 'neutral', name: 'Neutral', children: null, parentId: 0}
+      }
+    };
+    data.children.negative.children.negative1.parent = data.children.negative;
+    data.children.negative.children.negative2.parent = data.children.negative;
+    console.log(data);
     return (
       <div className="App">
         <header className="App-header">
@@ -17,19 +52,11 @@ class App extends Component {
         <div className="App-body">
           <InputBox
             name="Tone"
-            filter={{
-              id: 0,
-              name: 'Tone',
-              children: {
-                negative: {id: 'negative', name: 'Negative', children: null, parentId: 0},
-                positive: {id: 'positive', name: 'Positive', children: null, parentId: 0},
-                neutral: {id: 'neutral', name: 'Neutral', children: null, parentId: 0}
-              }
-            }}
+            filter={data}
             multipleSelect={false}
             clearall
             search
-            direction="horizontal"
+            direction="vertical"
             checkFilter={(name, filter, checked) => {
               console.log(this.state, name, filter, checked);
             }}
