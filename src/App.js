c84492563d33a186d31c089c31cf639ab9d381a6
Tuyen <dtuyen.le@gmail.com>
2018-01-20 16:34:07 -0500

    initial wrap up

--- src/App.js
+++ src/App.js
@@ -1,65 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import InputBox from 'components/InputBox';
+import ReactSelectionTree from 'components/ReactSelectionTree';
 
 class App extends Component {
   render() {
-    const data = {
-      id: 0,
-      name: 'Tone',
-      children: {
-        negative: {
-          id: 'negative',
-          name: 'Negative',
-          children: {
-            negative1: {
-              id: 'negative1',
-              name: 'Negative1',
-              children: null,
-              parentId: 'negative'
-            },
-            negative2: {
-              id: 'negative2',
-              name: 'Negative2',
-              children: null,
-              parentId: 'negative'
-            }
-          },
-          parentId: 0
-        },
-        positive: {
-          id: 'positive',
-          name: 'Positive',
-          children: null,
-          parentId: 0
-        },
-        neutral: {id: 'neutral', name: 'Neutral', children: null, parentId: 0}
-      }
-    };
-    data.children.negative.children.negative1.parent = data.children.negative;
-    data.children.negative.children.negative2.parent = data.children.negative;
-    console.log(data);
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Selection Tree</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <div className="App-body">
-          <InputBox
-            name="Tone"
-            filter={data}
-            multipleSelect={false}
-            clearall
-            search
-            direction="vertical"
-            checkFilter={(name, filter, checked) => {
-              console.log(this.state, name, filter, checked);
-            }}
+          <ReactSelectionTree
+            name="Location"
+            data={{"children":{"4000":{"children":{"4001":{"children":{"4010":{"name":"Scheduling","id":4010,"type":"Reference","parentId":4001},"4011":{"name":"Appointments","id":4011,"type":"Reference","parentId":4001},"4012":{"name":"Parking","id":4012,"type":"Reference","parentId":4001},"4013":{"name":"Location","id":4013,"type":"Reference","parentId":4001},"4014":{"name":"Getting Timely Care","id":4014,"type":"Reference","parentId":4001}},"name":"Access","id":4001,"type":"Reference","parentId":4000},"4002":{"children":{"4021":{"children":{"4022":{"name":"Medical Staff/Nurse","id":4022,"type":"Reference","parentId":4021},"4023":{"name":"Technologist","id":4023,"type":"Reference","parentId":4021}},"name":"Clinical Staff","id":4021,"type":"Reference","parentId":4002},"4024":{"name":"Materials (Brochures, Information, Reports, etc.)","id":4024,"type":"Reference","parentId":4002},"4025":{"name":"Follow Up","id":4025,"type":"Reference","parentId":4002},"4015":{"children":{"4016":{"name":"Helps Patients Understand","id":4016,"type":"Reference","parentId":4015},"4017":{"name":"Listens & Answers Questions","id":4017,"type":"Reference","parentId":4015},"4018":{"name":"Time Spent with Patient","id":4018,"type":"Reference","parentId":4015},"4019":{"name":"Bedside Manner","id":4019,"type":"Reference","parentId":4015},"4020":{"name":"Knowledge & Skill","id":4020,"type":"Reference","parentId":4015}},"name":"Physician","id":4015,"type":"Reference","parentId":4002}},"name":"Communication","id":4002,"type":"Reference","parentId":4000},"4003":{"children":{"4032":{"children":{"4033":{"name":"Scheduling Staff","id":4033,"type":"Reference","parentId":4032},"4034":{"name":"Front-Desk Staff","id":4034,"type":"Reference","parentId":4032}},"name":"Office Staff","id":4032,"type":"Reference","parentId":4003},"4035":{"name":"Billing","id":4035,"type":"Reference","parentId":4003},"4036":{"name":"Registration","id":4036,"type":"Reference","parentId":4003},"4026":{"children":{"4027":{"name":"Noise","id":4027,"type":"Reference","parentId":4026},"4028":{"name":"Lighting","id":4028,"type":"Reference","parentId":4026},"4029":{"name":"Cleanliness","id":4029,"type":"Reference","parentId":4026},"4030":{"name":"Comfort","id":4030,"type":"Reference","parentId":4026},"4031":{"name":"Temperature","id":4031,"type":"Reference","parentId":4026}},"name":"Facilities/Environment","id":4026,"type":"Reference","parentId":4003}},"name":"Office","id":4003,"type":"Reference","parentId":4000},"4004":{"children":{"4037":{"name":"Diagnosis","id":4037,"type":"Reference","parentId":4004},"4038":{"name":"Quality of Procedure","id":4038,"type":"Reference","parentId":4004}},"name":"Outcome","id":4004,"type":"Reference","parentId":4000},"4005":{"children":{"4039":{"name":"Likely to Recommend","id":4039,"type":"Reference","parentId":4005},"4040":{"name":"Likely to Come Back","id":4040,"type":"Reference","parentId":4005}},"name":"Loyalty","id":4005,"type":"Reference","parentId":4000}},"name":"Patient Satisfaction","id":4000,"type":"Reference","parentId":0}},"name":"Category","parentId":null}}
+            breadcrumClick={(data) => {}}
+            check={(data) => {}}
+            search={(data) => {}}
+            reset={(data) => {}}
+            uncheckAll={(data) => {}}
+            showSearch
+            showClearAll
+            infinite
+            multipleSelect
           />
         </div>
       </div>
